b4ca348faaaf9d1df3edc6869cc5f54e5f9a3a8d
karansingh-amagi <karan.singh@amagi.com>
2023-02-03 19:13:34 +0530

    implemented git helper functions in python as per requirements

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from git import Repo
+import shutil
+import os
+
+class GithubHelper:
+    def __init__(self, repo_name, username, password) -> None:
+        self.username = username
+        self.password = password
+        self.repo_name = repo_name
+
+        # self.remote = "https://{username}:{password}@github.com/amagimedia/{repo_name}.git".format(username, password, repo_name)
+        self.remote = "https://{}:{}@github.com/karansingh-amagi/{}.git".format(username, password, repo_name)
+
+
+    def clone_and_copy(self, copy_dir):
+        if os.path.isdir(self.repo_name):
+            shutil.rmtree(os.path.join(os.getcwd(), self.repo_name))
+
+        Repo.clone_from(self.remote, self.repo_name)
+        dest = str(os.path.join(os.getcwd(), self.repo_name))
+        shutil.copytree(copy_dir, dest, dirs_exist_ok=True, ignore=shutil.ignore_patterns('.*', '_*'))
+
+
+    def commit_push_delete(self, commit_msg: str, branch_name: str):
+        repo = Repo(self.repo_name)
+        repo.git.checkout('-B', branch_name)
+        repo.git.add(".")
+        repo.git.commit(m=commit_msg)
+        repo.git.push('--set-upstream', repo.remote().name, branch_name)
+        
+        shutil.rmtree(os.path.join(os.getcwd(), self.repo_name))
+
+
+gh = GithubHelper('github-helper', "karansingh-amagi", "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")
+gh.clone_and_copy("/home/karans/tasks/github-helper/src/copy_test_dir")
+gh.commit_push_delete(commit_msg="commit from github helper", branch_name='main')
