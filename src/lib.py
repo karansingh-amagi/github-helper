691ec0e5a2a72443261f76fb74b27a683b775b3c
karansingh-amagi <karansingh@amagi.com>
2023-02-08 18:09:12 +0530

    Testing my code

--- src/lib.py
+++ src/lib.py
@@ -52,7 +52,7 @@ class GithubHelper:
             print("commit finished")
 
         except Exception as e:
-            pass
+            raise e
 
     def create_pr(self, title: str, body: str, head: str, base: str = "main") -> PullRequest:
         try:
